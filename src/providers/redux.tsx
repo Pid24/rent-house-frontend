bb6d71fb3ce04f1a365cb41ccd22a54851b5421a
Pid24 <rofidnasifannafie@gmail.com>
2026-02-02 06:10:33 +0700

    first commit

---
+++ src/providers/redux.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import { store } from "@/lib/store";
+import { Provider } from "react-redux";
+
+function ReduxProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      <Provider store={store}>{children}</Provider>
+    </SessionProvider>
+  );
+}
+
+export default ReduxProvider;
