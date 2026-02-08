4dee78fab8a2cd2e6065bd461ae8b2bde7f1ea90
Pid24 <rofidnasifannafie@gmail.com>
2026-02-08 07:00:55 +0700

    change storage disk to public for image upload

--- src/components/molecules/card/card-deals.tsx
+++ src/components/molecules/card/card-deals.tsx
@@ -3,26 +3,12 @@ import { moneyFormat } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
-function CardDeals({
-  image,
-  title,
-  slug,
-  price,
-  wide,
-  capacity,
-  wifi,
-}: DealsProps) {
+function CardDeals({ image, title, slug, price, wide, capacity, wifi }: DealsProps) {
   return (
     <Link href={slug}>
       <figure className="relative">
         {image ? (
-          <Image
-            src={image}
-            alt={title}
-            height={0}
-            width={0}
-            className="w-[220px] h-[310px] xl:w-[260px] xl:h-[350px] rounded-3xl object-cover bg-gray-300"
-          />
+          <Image src={`${process.env.NEXT_PUBLIC_STORAGE_BASE_URL}/${image}`} alt={title} height={0} width={0} className="w-[220px] h-[310px] xl:w-[260px] xl:h-[350px] rounded-3xl object-cover bg-gray-300" unoptimized />
         ) : (
           <div className="w-[220px] h-[310px] xl:w-[260px] xl:h-[350px] rounded-3xl object-cover bg-gray-300" />
         )}
@@ -31,57 +17,29 @@ function CardDeals({
           <div className="mt-auto">
             <div className="flex items-center justify-between">
               <div>
-                <span className="block font-bold text-xl leading-[30px]">
-                  {title}
-                </span>
+                <span className="block font-bold text-xl leading-[30px]">{title}</span>
                 <span className="block text-sm leading-[21px]">
-                  <span className="font-semibold">
-                    {moneyFormat.format(price)}
-                  </span>
+                  <span className="font-semibold">{moneyFormat.format(price)}</span>
                   /mo
                 </span>
               </div>
               <div className="flex items-center text-sm leading-[21px]">
-                <Image
-                  src="/icons/star.svg"
-                  alt="star-icon"
-                  height={0}
-                  width={0}
-                  className="w-5 h-5 mr-[0.5px] -mt-1"
-                />
+                <Image src="/icons/star.svg" alt="star-icon" height={0} width={0} className="w-5 h-5 mr-[0.5px] -mt-1" />
                 4/5
               </div>
             </div>
 
             <div className="flex items-center justify-between mt-[14px]">
               <div className="flex items-center text-sm leading-[21px]">
-                <Image
-                  src="/icons/format-square.svg"
-                  alt="square-icon"
-                  height={0}
-                  width={0}
-                  className="w-5 h-5 mr-1"
-                />
+                <Image src="/icons/format-square.svg" alt="square-icon" height={0} width={0} className="w-5 h-5 mr-1" />
                 {wide} sqft
               </div>
               <div className="flex items-center text-sm leading-[21px]">
-                <Image
-                  src="/icons/profile-2user.svg"
-                  alt="profile-icon"
-                  height={0}
-                  width={0}
-                  className="w-5 h-5 mr-1"
-                />
+                <Image src="/icons/profile-2user.svg" alt="profile-icon" height={0} width={0} className="w-5 h-5 mr-1" />
                 {capacity}
               </div>
               <div className="flex items-center text-sm leading-[21px]">
-                <Image
-                  src="/icons/wifi.svg"
-                  alt="wifi-icon"
-                  height={0}
-                  width={0}
-                  className="w-5 h-5 mr-1"
-                />
+                <Image src="/icons/wifi.svg" alt="wifi-icon" height={0} width={0} className="w-5 h-5 mr-1" />
                 {wifi}gb
               </div>
             </div>
